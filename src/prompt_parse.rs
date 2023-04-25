e23db734036b74e588bb978d95efcaa41a6c0e9d
Benjamin Woodruff <github@benjam.info>
2023-04-24 18:24:05 -0700

    Add support for style presets

--- src/prompt_parse.rs
+++ src/prompt_parse.rs
@@ -1,10 +1,45 @@
+use anyhow::Context;
 use once_cell::sync::Lazy;
 use regex::Regex;
+use std::collections::HashSet;
 
 use crate::stability_api::{TextPrompt, TextToImageRequest};
 
 static MODIFIER_RE: Lazy<Regex> =
-    Lazy::new(|| Regex::new("![a-zA-Z_]+\\b").expect("regex compilation"));
+    Lazy::new(|| Regex::new("!([a-zA-Z_-]+)\\b").expect("regex compilation"));
+
+static STYLE_PRESETS_VEC: Lazy<Vec<&'static str>> = Lazy::new(|| {
+    vec![
+        "3d-model",
+        "analog-film",
+        "anime",
+        "cinematic",
+        "comic-book",
+        "digital-art",
+        "enhance",
+        "fantasy-art",
+        "isometric",
+        "line-art",
+        "low-poly",
+        "modeling-compound",
+        "neon-punk",
+        "origami",
+        "photographic",
+        "pixel-art",
+        "tile-texture",
+    ]
+});
+
+static STYLE_PRESETS_SET: Lazy<HashSet<&'static str>> =
+    Lazy::new(|| STYLE_PRESETS_VEC.iter().copied().collect());
+
+static STYLE_PRESETS_STRING: Lazy<String> = Lazy::new(|| {
+    STYLE_PRESETS_VEC
+        .iter()
+        .map(|preset| format!("!{preset}"))
+        .collect::<Vec<_>>()
+        .join(", ")
+});
 
 pub fn prompt_parse(msg: &str) -> anyhow::Result<TextToImageRequest<'_>> {
     let mut request = TextToImageRequest {
@@ -18,17 +53,32 @@ pub fn prompt_parse(msg: &str) -> anyhow::Result<TextToImageRequest<'_>> {
         ..Default::default()
     };
 
-    for modifier in MODIFIER_RE.find_iter(msg) {
-        match modifier.as_str() {
-            "!quality" => {
-                request.samples = Some(1);
-                request.steps = Some(100);
+    for capture in MODIFIER_RE.captures_iter(msg) {
+        let modifier = capture
+            .get(1)
+            .context("regex capture should contain group 1")?;
+        let modifier = modifier.as_str();
+        if let Some(style_preset) = STYLE_PRESETS_SET.get(modifier) {
+            if request.style_preset.is_some() {
+                anyhow::bail!("only one style preset is allowed at once");
             }
-            "!strict" => {
-                request.cfg_scale = Some(15.0);
-            }
-            mod_str => {
-                anyhow::bail!("{mod_str} is not a supported modifier (!quality, !strict)");
+            request.style_preset = Some(style_preset); // use the 'static version
+        } else {
+            match modifier {
+                "quality" => {
+                    request.samples = Some(1);
+                    request.steps = Some(100);
+                }
+                "strict" => {
+                    request.cfg_scale = Some(15.0);
+                }
+                mod_str => {
+                    anyhow::bail!(
+                        "{mod_str} is not a supported modifier (!quality, !strict) \
+                        or style preset ({})",
+                        *STYLE_PRESETS_STRING,
+                    );
+                }
             }
         }
     }
